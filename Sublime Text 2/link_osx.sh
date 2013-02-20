ab069e2ea4c51f12d132105f3b9d89fbf3889d1e
G. Jocelyn <eri@erioxyde.com>
2013-02-21 00:47:15 +0100

    Update the deploy script for Sublime Text 2 on OSX.

--- Sublime Text 2/link_osx.sh
+++ Sublime Text 2/link_osx.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
-## link.sh for  in /Users/jocelyn/Dropbox/AppsConfiguration/Sublime Text 2
-##
-## Made by Jocelyn GIRARD
-## Login   <jocelyn@smartnsoft.com>
-##
-## Started on  Wed Feb 13 11:18:03 2013 Jocelyn GIRARD
-## Last update Wed Feb 13 11:23:38 2013 Jocelyn GIRARD
-##
 
 CONF=`pwd`
 
